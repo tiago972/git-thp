843bdad0541ea8428aac9ff9feac641ced7fcae1
Edouard Baudouin <Tiago@MacBook-Air-de-Edouard.local>
2019-01-03 22:27:48 +0100

    mauvaise compréhension d\'une consigne

--- ruby/vendredi/exo_16.rb
+++ ruby/vendredi/exo_16.rb
@@ -1,8 +1,9 @@
-puts "T'es né quand ?"
+puts "T'as quel âge ?"
 print "> "
 num_var = gets.to_i
-num_2 = num_var
-while num_var < 2019
-	puts "Il y a #{2019 - num_var} ans tu avais #{num_var - num_2} ans"
-	num_var+=1
+num_2 = 0
+while num_var >= 0
+	puts "Il y a #{num_var} ans, tu avais #{num_2} ans"
+	num_var-=1
+	num_2+=1
 end
--- ruby/vendredi/exo_17.rb
+++ ruby/vendredi/exo_17.rb
@@ -1,12 +1,13 @@
-puts "T'es né quand ?"
+puts "T'as quel âge ?"
 print "> "
 num_var = gets.to_i
-num_2 = num_var
-while num_var < 2019
-	if (2019 - num_var == num_var - num_2)
-		puts "Il y a #{2019 - num_var} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
+num_2 = 0
+while num_var >= 0
+	if num_var != num_2
+		puts "Il y a #{num_var} ans, tu avais #{num_2} ans"
 	else
-		puts "Il y a #{2019 - num_var} ans tu avais #{num_var - num_2} ans"
+		puts "Il y a #{num_var} ans, tu avais la moitié de l'âge que tu as aujourd'hui"
 	end
-	num_var+=1
+	num_var-=1
+	num_2+=1
 end
