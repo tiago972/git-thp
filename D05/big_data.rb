1fb64d4786d6f997673c68c3a15d1e013c3d70d8
tiago972 <edouard.baudouin@hotmail.fr>
2019-01-10 20:50:49 +0100

    bientôt fini

--- D05/big_data.rb
+++ D05/big_data.rb
@@ -12,6 +12,45 @@ def start_nb(var_array)
 	return i
 end
 
+def letters_Aude(var_array)
+	i = 0
+	var_array.each do |j|
+		if !(j !~ /aude/i)
+			i+=1
+		end
+	end
+	return i
+end
+
+def start_upper(var_array)
+	i = 0
+	var_array.each do |j|
+		if !(j !~ /^@[A-Z]/)
+			i+=1
+		end
+	end
+	return i
+end
+
+def have_upper(var_array)
+	i = 0
+	var_array.each do |j|
+		if !(j !~ /[A-Z]/)
+			i+=1
+		end
+	end
+	return i
+end
+
+def nb_underscore(var_array)
+	count_array = []
+	i = 0
+	var_array.each do |j|
+			i = i + j.scan(/_/).size
+	end
+	return i
+end
+
 def call_function(var_int, var_array)
 	case var_int
 	when 1
@@ -23,23 +62,24 @@ def call_function(var_int, var_array)
 		puts "*****************"
 		program_choose(var_array)
 	when 3
-		puts "#{start_nb(var_array)}"
+		puts "Il y a #{letters_Aude(var_array)} personnes ayant ces 4 lettres dans leurs handle"
 		puts "*****************"
 		program_choose(var_array)
 	when 4
-		puts "#{start_nb(var_array)}"
+		puts "Il y #{start_upper(var_array)} personnes dont le handle commencent par une majuscule"
 		puts "*****************"
 		program_choose(var_array)
 	when 5
-		puts "#{start_nb(var_array)}"
+		puts "Il y a #{have_upper(var_array)} personnes dont le handle contient une majuscule"
 		puts "*****************"
 		program_choose(var_array)
 	when 6
-		puts "#{start_nb(var_array)}"
+		puts "Il ya #{nb_underscore(var_array)} underscore"
 		puts "*****************"
 		program_choose(var_array)
 	when 7
-		puts "#{start_nb(var_array)}"
+		puts "liste triée par ordre alphabétix (à l'aide de son pote cétotaumatix .sort)"
+		puts "#{var_array.sort}"
 		puts "*****************"
 		program_choose(var_array)
 	when 8
@@ -55,15 +95,15 @@ end
 
 def program_choose(var_array)
 	puts "Choisis ce que tu veux afficher de ton tableau:
-	1 pour combien y a-t-il de journalistes dans ce array ? 
-	2 pour combien d'handle contiennent un un numéro?
-	3 pour combien d'handle contiennent les 4 lettres du prénom \"Aude\" à la suite (sans prendre en compte les majuscules?)?
-	4 pour combien commencent par une majuscule?
-	5 combien contiennent une majuscule ?
-	6 combient y a t'il de underscore dans tous les pseudos confondus?
-	7 pour Trier la liste par ordre alphabétique
-	8 pour quels sont les 50 handle les plus courts de ce array
-	9 quelle est la position dans l'array de la personne @epenser?
+	1 Pour combien y a-t-il de journalistes dans ce array ? 
+	2 Pour combien d'handle contiennent un un numéro?
+	3 Pour combien d'handle contiennent les 4 lettres du prénom \"Aude\" à la suite (sans prendre en compte les majuscules?)?
+	4 Pour combien commencent par une majuscule?
+	5 Combien contiennent une majuscule ?
+	6 Combient y a t'il de underscore dans tous les pseudos confondus?
+	7 Pour trier la liste par ordre alphabétique
+	8 Pour quels sont les 50 handle les plus courts de ce array
+	9 Quelle est la position dans l'array de la personne @epenser?
 	*************************************************************	
 	N'importe quelle touche pour quitter"
 	print ('> ')
