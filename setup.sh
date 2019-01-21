b4ea71a422c57890d8354812009843559876432d
tiago972 <edouard.baudouin@hotmail.fr>
2019-01-21 19:18:13 +0100

    I don't give a damn 'bout my reputation

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+rspec --ini
+mkdir lib
+mkdir lib/app
+mkdir lib/views
+mkdir db
+echo "require \'bundler\'
+Bundler.require" > app.rb
+touch README.md
+echo "source \"https://rubygems.org\"
+ruby '2.5.1'
+gem 'rspec'
+gem 'pry'
+gem 'rubocop', '~> 0.57.2'
+gem 'watir'
+gem 'nokogiri'" > Gemfile
+touch .env
+echo ".env" > .gitignore
